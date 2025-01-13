0bf2e68ab71d1694977d8e2d074164ee5dc23dae
Groggie <groggy67@yahoo.com>
2025-01-13 10:03:56 +0000

    Added outputs.tf to indicate loaclstack is being used (is_localstack = true)

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+# Detect if Terraform configuration 
+# is deployed against LocalStack,
+
+data "aws_caller_identity" "current" {}
+output "is_localstack" {
+  value = data.aws_caller_identity.current.id == "000000000000"
+}
