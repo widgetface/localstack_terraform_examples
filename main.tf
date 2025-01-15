43b2b3f011987a5a8f662c0affab5ec72ff154bd
Groggie <groggy67@yahoo.com>
2025-01-15 10:33:52 +0000

    Refactor main.tf and outputs to use apigateway modeule and load openapi spec from a file path

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
 module "s3_upload" {
   source           = "./modules/s3_private_file_upload"
-  fileset_path     = "./data"
+  fileset_path     = "./data/openapi"
   s3_bucket_prefix = "project-alpha-openapi-"
   s3_object_metadata = {
     "type" : "OpenAPI"
     "author" : "das@mycomp.com"
     "created" : "12/12/2024"
   }
+}
+
+module "apig" {
+  source           = "./modules/api_gateway_rest"
+  apig_name        = "project_alpha_apig_rest"
+  apig_description = "Project alpha API Gateway"
+  file_path        = "./data/openapi/test.json"
 }
--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,4 @@ output "is_localstack" {
   value = data.aws_caller_identity.current.id == "000000000000"
 }
 
-output "bucket_name" {
-  value       = module.s3_upload.bucket_name
-  description = "The name of the created S3 bucket"
-}
+
